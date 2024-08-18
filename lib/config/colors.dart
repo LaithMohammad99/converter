6ed563feebf5a384d148128c3edf35adc306d669
“Laith <laithalsahore19@gmail.com>
2024-08-18 13:29:42 +0300

    complete my app

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/animation.dart';
 Color primaryGreen = const Color(0xff41CAC2);
 Color lightGreen = const Color(0xff72F3EC);
 Color darkGreen = const Color(0xff36ABA4);
+Color darkGreen2 = const Color(0xff36ABA2);
