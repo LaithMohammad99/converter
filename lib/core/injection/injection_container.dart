ae824f92ac666320cce14e543235b8f882f15566
“Laith <laithalsahore19@gmail.com>
2024-08-19 10:05:48 +0300

    re structure file

--- lib/core/injection/injection_container.dart
+++ lib/core/injection/injection_container.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:taske_bloc_converte/core/base_app_client/base_app_client.dart';
+import 'package:taske_bloc_converte/core/service/navigation_service.dart';
 import 'package:taske_bloc_converte/modules/converter/Bloc/bloc.dart';
 import 'package:taske_bloc_converte/modules/historical/bloc/historical_bloc.dart';
-import 'package:taske_bloc_converte/service/navigation_service.dart';
 
 GetIt di = GetIt.instance;
 
